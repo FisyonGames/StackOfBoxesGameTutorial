7af716de40e25c20893757dbfbc3961e1500b57e
FisyonGames <fisyon.games@gmail.com>
2022-03-23 21:24:18 +0300

    Added movement scripts

---
+++ StackOfBoxesGameTutorial/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] [Range(5,25)] private float movementSpeed = 20f;
+    [SerializeField] [Range(5,25)] private float speedForLeftRightMovement = 20f;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        //transform.position += Vector3.forward * movementSpeed * Time.deltaTime;
+
+        if(Input.GetKey(KeyCode.LeftArrow) || MobileInput.Instance.swipeLeft)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speedForLeftRightMovement);
+        }
+        if(Input.GetKey(KeyCode.RightArrow) || MobileInput.Instance.swipeRight)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speedForLeftRightMovement);
+        }
+
+        // Eğer yan engeller yoksa...
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -4.1f, 4.1f);
+        transform.position = clampedPosition;
+    }
+
+}
